06de717865ef804892c1449cd84d1baadd8b3446
[cans1994] <cans.bl@hotmail.com>
2022-02-22 16:28:17 +0100

    second one

--- vue/head.php
+++ vue/head.php
@@ -15,4 +15,12 @@
       <a href="index.php?table=School_year">School Year</a>
     </div>
   </nav>
+  <script src="https://cdn.jsdelivr.net/npm/darkmode-js@1.5.7/lib/darkmode-js.min.js"></script>
+  <script>
+    function addDarkmodeWidget() {
+      new Darkmode().showWidget();
+    }
+    window.addEventListener('load', addDarkmodeWidget);
+  </script>
+
 </body>
